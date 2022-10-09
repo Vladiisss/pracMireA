e55753ad967a0d483728845de7b09127a201d21a
Vladiisss <vladislav-kur950@yandex.ru>
2022-10-09 18:02:58 +0300

    Comit2

--- src/PR4/Examples/NullLayout.java
+++ src/PR4/Examples/NullLayout.java
@@ -13,19 +13,19 @@ class NullLayout extends JFrame {
 
 
     public NullLayout() {
-        setLayout(null);
-        but1.setBounds(150, 300, 100, 20); // added at 150,300 width = 100, height=20
 
+        setSize(500, 500);
+        setLayout(null);
+
+        but1.setBounds(150, 300, 100, 20); // added at 150,300 width = 100, height=20
         but2.setSize(80, 400); // added at 0,0 width = 80, height=400
         but3.setLocation(300, 100);
-
-
         but3.setSize(200, 75);
-// those two steps can be combined in one setBounds method call
+
+        // those two steps can be combined in one setBounds method call
         add(but1);
         add(but2);
         add(but3);
-        setSize(500, 500);
     }
 
     public static void main(String[] args) {
