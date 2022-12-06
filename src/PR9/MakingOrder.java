ef5cd5f21a9e1b5cf6eaed3d3e8c32c4317c2806
Vladiisss <vladislav-kur950@yandex.ru>
2022-12-06 14:10:38 +0300

    Comit3

---
+++ src/PR9/MakingOrder.java
@@ -0,0 +1,46 @@
+package PR9;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MakingOrder {
+
+    private static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static void checkINN(String inn) throws BadINNException {
+
+        if (inn.length() != 10) {
+            throw new BadINNException("Неверное кол-во символов");
+        }
+        if (!isNumeric(inn)) {
+            throw new BadINNException("INN содержит буквы");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите имя");
+        String name = in.nextLine();
+        System.out.println("Введите INN:");
+        String inn = in.nextLine();
+
+        try {
+            checkINN(inn);
+        } catch (BadINNException e) {
+            System.out.println(e);
+        }
+
+
+
+    }
+}
