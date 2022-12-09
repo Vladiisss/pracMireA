4ef9182fdb57a79b51a1b8860b6468db0184a970
Vladiisss <vladislav-kur950@yandex.ru>
2022-12-10 00:13:44 +0300

    Comit

---
+++ src/PR10/Complex.java
@@ -0,0 +1,4 @@
+package PR10;
+
+public record Complex(int real, int image) {
+}
---
+++ src/PR10/ComplexAbstractFactory.java
@@ -0,0 +1,7 @@
+package PR10;
+
+public interface ComplexAbstractFactory {
+
+    Complex createComplex();
+    Complex createComplex(int real, int image);
+}
